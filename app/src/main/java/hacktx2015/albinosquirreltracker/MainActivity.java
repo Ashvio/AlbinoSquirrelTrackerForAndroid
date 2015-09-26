b5f913f34f017ce5a4c02ca3785ec4ff26553675
Jaydeep <jaydeep.singh.424@gmail.com>
2015-09-26 18:50:09 -0500

    added the UI, basic functionality

--- app/src/main/java/hacktx2015/albinosquirreltracker/MainActivity.java
+++ app/src/main/java/hacktx2015/albinosquirreltracker/MainActivity.java
@@ -1,16 +1,39 @@
 package hacktx2015.albinosquirreltracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private EditText descriptionEditText;
+
+    //location
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        descriptionEditText = (EditText) findViewById(R.id.descriptionEditText);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    public void buttonPressed(View view){
+        //launch camera activity
+
+
+        //do this after saving camera
+        //locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, null);
+
     }
 
 
@@ -34,5 +57,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("mainActivity","location found at: " + location.getLatitude() + " " +location.getLongitude() + " Description is: " + descriptionEditText.getText());
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
